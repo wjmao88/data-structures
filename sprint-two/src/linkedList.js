cb38e535531d0ae5270cd66f86bdf6a93c07140b
wjmao88 <wjmao88@gmail.com>
2014-02-26 12:48:23 -0800

    implemented linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if (this.head === null){
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function(){
+    var result = this.head;
+    if (this.head !== null){
+      this.head = this.head.next;
+    }
+    return result;
   };
 
   list.contains = function(target, node){
+    node = node || this.head;
+    if (node === null){
+      return false;
+    }
+    if (node.value === target){
+      return true;
+    }
+    if (node === list.tail){
+      return false;
+    }
+    return this.contains(target, node.next);
   };
 
   return list;
